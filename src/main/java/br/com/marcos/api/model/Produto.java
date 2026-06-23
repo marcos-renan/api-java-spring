d4417470cbfdbd01cbd23dfdc6c5fee2de3a8fcf
marcos-renan <devmarcos7@gmail.com>
2026-06-23 10:07:08 -0300

    feat: ✨ adicionando mapeamento da entidade produto.

--- src/main/java/br/com/marcos/api/model/Produto.java
+++ src/main/java/br/com/marcos/api/model/Produto.java
@@ -1,10 +1,25 @@
 package br.com.marcos.api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @Column
     private  String id;
+
+    @Column
     private String nome;
+
+    @Column
     private String descricao;
+
+    @Column
     private Double preco;
 
     public String getId() {
